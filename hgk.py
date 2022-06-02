be63cfa17ea236df7d6ee14e1bb95c0b781a5394
notghettolenny <79231993+notghettolenny@users.noreply.github.com>
2022-06-02 23:42:21 +0100

    Add files via upload

---
+++ hgk.py
@@ -0,0 +1,176 @@
+
+
+# Commented out IPython magic to ensure Python compatibility.
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps.zip \
+    -O /tmp/rps.zip
+  
+!wget --no-check-certificate \
+    https://storage.googleapis.com/laurencemoroney-blog.appspot.com/rps-test-set.zip \
+    -O /tmp/rps-test-set.zip
+
+################################################################################
+
+import os
+import zipfile
+
+local_zip = '/tmp/rps.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/')
+zip_ref.close()
+
+local_zip = '/tmp/rps-test-set.zip'
+zip_ref = zipfile.ZipFile(local_zip, 'r')
+zip_ref.extractall('/tmp/')
+zip_ref.close()
+
+################################################################################
+
+rock_dir = os.path.join('/tmp/rps/rock')
+paper_dir = os.path.join('/tmp/rps/paper')
+scissors_dir = os.path.join('/tmp/rps/scissors')
+
+print('total training rock images:', len(os.listdir(rock_dir)))
+print('total training paper images:', len(os.listdir(paper_dir)))
+print('total training scissors images:', len(os.listdir(scissors_dir)))
+
+rock_files = os.listdir(rock_dir)
+print(rock_files[:10])
+
+paper_files = os.listdir(paper_dir)
+print(paper_files[:10])
+
+scissors_files = os.listdir(scissors_dir)
+print(scissors_files[:10])
+
+#################################################################"############
+
+# %matplotlib inline
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+pic_index = 2
+
+next_rock = [os.path.join(rock_dir, fname) 
+                for fname in rock_files[pic_index-2:pic_index]]
+next_paper = [os.path.join(paper_dir, fname) 
+                for fname in paper_files[pic_index-2:pic_index]]
+next_scissors = [os.path.join(scissors_dir, fname) 
+                for fname in scissors_files[pic_index-2:pic_index]]
+
+for i, img_path in enumerate(next_rock+next_paper+next_scissors):
+  #print(img_path)
+  img = mpimg.imread(img_path)
+  plt.imshow(img)
+  plt.axis('Off')
+  plt.show()
+
+"""# Model"""
+
+import tensorflow as tf
+import keras_preprocessing
+from keras_preprocessing import image
+from keras_preprocessing.image import ImageDataGenerator
+
+TRAINING_DIR = "/tmp/rps/"
+training_datagen = ImageDataGenerator(
+      rescale = 1./255,
+	    rotation_range=40,
+      width_shift_range=0.2,
+      height_shift_range=0.2,
+      shear_range=0.2,
+      zoom_range=0.2,
+      horizontal_flip=True,
+      fill_mode='nearest')
+
+VALIDATION_DIR = "/tmp/rps-test-set/"
+validation_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_generator = training_datagen.flow_from_directory(
+	TRAINING_DIR,
+	target_size=(150,150),
+	class_mode='categorical',
+  batch_size=126
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+	VALIDATION_DIR,
+	target_size=(150,150),
+	class_mode='categorical',
+  batch_size=126
+)
+
+# https://machinelearningmastery.com/padding-and-stride-for-convolutional-neural-networks/
+model = tf.keras.models.Sequential([
+    # Note the input shape is the desired size of the image 150x150 with 3 bytes color
+    # This is the first convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(150, 150, 3)), # 64 hidden layers with filter shape 3pixels*3pixels = weights of the filter (number of param is weights of the filter and the weight of the bias)
+                                                                                     # if we have input (8*8) and the filter is (3*3) the output will be (6*6)
+                                                                                     # padding is adding a border around the outside of the image. If if the input is 8*8, and the padding is 1, then the output is 10*10
+                                                                                     # Stride is the filter move change
+    tf.keras.layers.MaxPooling2D(2, 2),
+    # The second convolution
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The third convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # The fourth convolution
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    # Flatten the results to feed into a DNN
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dropout(0.5),
+    # 512 neuron hidden layer
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(3, activation='softmax')
+])
+
+model.summary()
+
+model.compile(loss = 'categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+history = model.fit(train_generator, epochs=25, steps_per_epoch=20, validation_data = validation_generator, verbose = 1, validation_steps=3)
+
+model.save("rps.h5")
+
+"""# Viz"""
+
+import matplotlib.pyplot as plt
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend(loc=0)
+plt.figure()
+
+
+plt.show()
+
+"""# Test"""
+
+import numpy as np
+from google.colab import files
+from keras.preprocessing import image
+
+uploaded = files.upload()
+
+for fn in uploaded.keys():
+ 
+  # predicting images
+  path = fn
+  img = image.load_img(path, target_size=(150, 150))
+  x = image.img_to_array(img)
+  x = np.expand_dims(x, axis=0)
+
+  images = np.vstack([x])
+  classes = model.predict(images, batch_size=10)
+  print(fn)
+  print(classes)
